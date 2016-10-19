d1aa6a2462cc1d81b403ff8f30798031df64ec1e
rolenweb <rolenweb@yandex.ru>
2016-10-19 15:15:45 +0300

    counter

--- wp-content/themes/realistic/footer.php
+++ wp-content/themes/realistic/footer.php
@@ -14,6 +14,18 @@
 		<div class="site-info mdl-mega-footer--bottom-section">
 			<?php realistic_copyrights(); ?>
 		</div><!-- .site-info -->	
+		<div>
+			<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='//www.liveinternet.ru/click' "+
+"target=_blank><img src='//counter.yadro.ru/hit?t44.5;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet' "+
+"border='0' width='31' height='31'><\/a>")
+//--></script><!--/LiveInternet-->
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- .mdl-layout -->
 <?php wp_footer(); ?>
